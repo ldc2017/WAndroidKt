1fea0aeb7e6c3f73ab8ae71af9b88321e0f312f1
ldc <ldc456@126.com>
2020-04-07 00:12:21 +0800

    `新增fragmentation 配置`

--- app/src/main/java/com/ldc/wandroidkt/mApp.kt
+++ app/src/main/java/com/ldc/wandroidkt/mApp.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.multidex.MultiDex
 import com.blankj.utilcode.util.Utils
 import com.ldc.wandroidkt.commom.ErrorHandle
+import me.yokeyword.fragmentation.Fragmentation
 
 class mApp : Application() {
 
@@ -16,6 +17,12 @@ class mApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
+        //
+        Fragmentation.builder()
+            .stackViewMode(Fragmentation.BUBBLE)
+            .debug(BuildConfig.DEBUG)
+            .install()
+        //
         Thread.setDefaultUncaughtExceptionHandler(ErrorHandle())
     }
 
